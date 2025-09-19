4270e886bab132d8e71664c1c711a6d44641943c
mohitsrivastava18 <mohitsrivastava.bhu@gmail.com>
2025-09-20 01:20:03 +0530

    Update TodoItem.jsx

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useTodo } from '../Contexts/TodoContext';
+import { useTodo } from '../Contexts';
 
 function TodoItem({ todo }) {
   const [isTodoEditable, setIsTodoEditable] = useState(false)
